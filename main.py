a31fec6d87ac770a95f47b6196ae43a405c72563
Gavin D'Hondt <103293120+declspecl@users.noreply.github.com>
2025-03-23 09:41:39 -0400

    DuckDuckCode: Create two APIs in my flash app: - One that finds

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+def gcd(a, b): #Greatest Common Divisor
+    while(b):
+        a, b = b, a % b
+    return a
+
+def lcm(a, b): #Lowest Common Multiple
+    return (a * b) // gcd(a, b)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/lcm/<int:num1>/<int:num2>')
+def get_lcm(num1, num2):
+    result = lcm(num1, num2)
+    return jsonify({'lcm': result})
+
+@app.route('/gcd/<int:num1>/<int:num2>')
+def get_gcd(num1, num2):
+    result = gcd(num1, num2)
+    return jsonify({'gcd': result})
 
 if __name__ == "__main__":
     app.run(debug=True)
